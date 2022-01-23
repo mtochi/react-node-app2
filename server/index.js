ec0854f42af83d73e9cf6473c2bc99fccc04ed8a
medinatochi <medinatoci@hotmail.com>
2022-01-23 15:18:08 +0100

    included path

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 //Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../client/build')));
+
 app.get("/api", (req, res) => {
     res.json({message: "Hello from server!"});
 })
